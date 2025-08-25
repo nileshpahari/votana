f27ea96c8f37968f0854729d6305bbfcbc633ca5
Nilesh Pahari <nileshpahari@protonmail.com>
2025-08-25 16:48:06 +0530

    add custom errors

---
+++ contract/programs/votana/src/errors/mod.rs
@@ -0,0 +1,39 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Poll counter cannot be less than zero")]
+    PollCounterUnderflow,
+    #[msg("Registrations counter cannot be less than zero")]
+    RegistrationsCounterUnderflow,
+    #[msg("Voter cannot vote twice")]
+    VoterAlreadyVoted,
+    #[msg("Candidate cannot register twice")]
+    CandidateAlreadyRegistered,
+    #[msg("Start date cannot be greater than end date")]
+    InvalidDates,
+    #[msg("Candidate is not in the poll")]
+    CandidateNotRegistered,
+    #[msg("Poll not currently active")]
+    PollNotActive,
+    #[msg("Poll does not exist or not found")]
+    PollDoesNotExist,
+    #[msg("Not authorized to perform this action")]
+    Unauthorized,
+    #[msg("Poll still active")]
+    PollStillActive,
+    #[msg("Name too long")]
+    NameTooLong,
+    #[msg("Description too long")] 
+    DescriptionTooLong,
+    #[msg("Title too long")] 
+    TitleTooLong,
+    #[msg("Votes counter underflow")]
+    VotesCounterUnderflow,
+    #[msg("Candidate votes underflow")]
+    CandidateVotesUnderflow,
+    #[msg("Voter not voted")]
+    VoterNotVoted,
+    #[msg("Voter candidate mismatch")]
+    VoterCandidateMismatch,
+}
